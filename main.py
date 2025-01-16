952a19c449e9b619a7eeb6b395d2365a1020f5ba
phenomonox <me@phenomonox.com>
2025-01-16 13:05:17 -0500

    Made a git ignore to ignore venv. But I added a window now. And gave it a way to be closed

--- main.py
+++ main.py
@@ -2,9 +2,21 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while 1 < 2:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill([0,0,0])
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
