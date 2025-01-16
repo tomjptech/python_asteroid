391b7f3113abd7fc6c1354f1ecdcf6855c045653
phenomonox <me@phenomonox.com>
2025-01-16 13:57:14 -0500

    added a clock to the python script so I can induce framerates. and I decided I should be more descriptive in the recreate_enivonrment shell script.

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    asteroids_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while 1 < 2:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill([0,0,0])
         pygame.display.flip()
+
+        dt = asteroids_clock.tick(60) / 1000
+        
+        
 
 if __name__ == "__main__":
     main()
--- recreate_environment.sh
+++ recreate_environment.sh
@@ -1,7 +1,7 @@
 #Recreate venv
 python3 -m venv venv
 
-#Activate it
+#Activate it. You will need to run this from project root in your WSL environment if you ever restart it. The front of your terminal should look something like this: (venv) <user>@<computer>:~/projects/github.com/tomjptech/python_asteroid$
 source venv/bin/activate
 
 #Install requirements
